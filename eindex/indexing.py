97db5afcdb8d56f3aea6fa2bd6a7f0822e3808f0
Callum McDougall <cal.s.mcdougall@gmail.com>
2023-08-31 21:20:00 +0100

    Improve documentation, fix bugs

--- eindex/indexing.py
+++ eindex/indexing.py
@@ -1,11 +1,24 @@
 import torch
 from typing import Union
+from collections import defaultdict
+
 from ._parsing import parse_string
 
 def eindex(
-    *tensors_and_pattern: Union[str, torch.Tensor]
+    *tensors_and_pattern: Union[str, torch.Tensor],
+    **kwargs,
 ):
     '''
+    Indexing inspired by einops notation: https://einops.rocks/
+
+    
+    ==========================================================================================
+    ======================================== EXAMPLES ========================================
+    ==========================================================================================
+
+    I've given 5 examples here. Most of the code below is annotated with explanations relating it
+    to one or more of these examples, to help explain what's going on.
+
     Some examples:
 
         # (1) You want to get all the logprobs for correct tokens
@@ -14,89 +27,170 @@ def eindex(
 
             pattern = "batch seq [batch seq]"
 
-        # (2) Same, but d_vocab_out is 2D not 1D for some reason
+        # (2a) Same, but d_vocab_out is 2D not 1D for some reason
             
             output[batch, seq] = logrobs[batch, seq, labels[batch, seq, 0], labels[batch, seq, 1]]
 
             pattern = "batch seq [batch seq 0] [batch seq 1]"
+        
+        # (2b) Using 2 tensors
+            
+            output[batch, seq] = logrobs[batch, seq, labels_1[batch, seq], labels_2[batch, seq]]
 
-        # (3) You want to get the logit lens at particular sequence positions (a different seq pos for each sequence in the batch)
+            pattern = "batch seq [batch seq] [batch seq]"
+
+        # (3) You want to get the logit lens at particular sequence positions (a different seq pos
+              for each sequence in the batch)
         
             output[batch, d_vocab] = logprobs[batch, labels[batch], d_vocab]
             
             pattern = "batch [batch] d_vocab"
 
+        # (4) You're indexing into a 2D tensor of tokens, for each destination token you're trying to get 5 source tokens
+              (i.e. indices[batch, seqQ, :] = the seqK positions of the top 5 source tokens)
+        
+            output[batch, seqQ, k] = tokens[batch, indices[batch, seqQ, k]]
+
+            pattern = "batch [batch seqQ k]"
+
+            
+    ==========================================================================================
+    ======================== ROUGH SUMMARY OF HOW THIS FUNCTION WORKS ========================
+    ==========================================================================================
 
     This is implemented explicitly, i.e. with no built-in PyTorch functions like `gather` (cause they confuse me lol). 
     For example, the way we implement (1) is by creating an index like this:
     
         logprobs[
-            t.arange(batch_size).reshape(batch_size, 1),
-            t.arange(seq_len).reshape(1, seq_len),
+            torch.arange(batch_size).reshape(batch_size, 1),
+            torch.arange(seq_len).reshape(1, seq_len),
             labels
         ]
 
     because when you index with tensors across different dimensions, they're all implicitly broadcast together. So we get:
 
         output[b, s] = logprobs[
-            t.arange(batch_size).reshape(batch_size, seq_len)[b, s],
-            t.arange(seq_len).reshape(batch_size, seq_len)[b, s],
+            torch.arange(batch_size).reshape(batch_size, seq_len)[b, s],
+            torch.arange(seq_len).reshape(batch_size, seq_len)[b, s],
             labels[b, s]
         ] = logprobs[b, s, labels[b, s]]
+
+    You can use `verbose=True` to print out the dimensions of the shape you'll get at the end.
     '''
 
     # Unpack and type-check arguments
-    arr, *indices, pattern = tensors_and_pattern
+    arr, *index_tensor_list, pattern = tensors_and_pattern
+    verbose = kwargs.pop("verbose", False)
+    assert len(kwargs) == 0, f"Unexpected keyword arguments: {kwargs.keys()}"
     assert isinstance(arr, torch.Tensor), "First argument must be a tensor."
-    assert all(isinstance(i, torch.Tensor) for i in indices), "All indices must be tensors."
+    assert all(isinstance(i, torch.Tensor) for i in index_tensor_list), "All indices must be tensors."
     assert isinstance(pattern, str), "Last argument must be a string."
 
+    # Parse the pattern string into a list of strings and lists
+    #   Example #1:  ['batch', 'seq', ['batch', 'seq']]
+    #   Example #2a: ['batch', 'seq', ['batch', 'seq', '0'], ['batch', 'seq', '1']]
     pattern_indices = parse_string(pattern)
+    pattern_indices_str = [p for p in pattern_indices if isinstance(p, str)]
+    pattern_indices_list = [p for p in pattern_indices if isinstance(p, list)]
 
     # Check the dimensions are appropriate
     assert len(pattern_indices) == arr.ndim, "Invalid indices. There should be as many terms (strings or square bracket expressions) as there are dims in the first argument (arr)."
 
-    full_idx = []
+    # Check whether you're doing #2a (using a single index with multiple slices) or #2b (using multiple indices), but not both!
+    using_multiple_indices = len(index_tensor_list) > 1
+    using_multiple_slices = any((isinstance(i, list) and any(j.isdigit() for j in i)) for i in pattern_indices)
+    assert not (using_multiple_indices and using_multiple_slices), "You can't use both multiple indices and multiple slices. Choose one or the other."
 
-    # Get dimensions of output, so we know what to broadcast our indices to (when they're strings)
-    pattern_indices_str = [p for p in pattern_indices if isinstance(p, str)]
-    output_shape = tuple([d for d, p in zip(arr.shape, pattern_indices) if isinstance(p, str)])
+    # Create a dicionary mapping names of dimensions to their sizes (purely based on the things that appear in square brackets)
+    #   Example #1: ['batch', 'seq', ['batch', 'seq']] -> {'batch': batch_size, 'seq': seq_len}
+    #   Example #4: ['batch', ['batch', 'seq', 'k']] -> {'batch': batch_size, 'seq': seq_len, 'k': k}
+    index_tensor_counter = 0
+    dimension_sizes = {}
+    for item in pattern_indices_list:
+        # Check this square brackets expression matches the indexing tensor that it corresponds to
+        assert (
+            len(item) == len(index_tensor_list[index_tensor_counter].shape),
+            "Invalid indices. There should be as many terms in each square brackets expression as the corresponding indexing tensor has dimensions."
+        )
+        # Once you've asserted that it does, add the dimension sizes to the dictionary (checking for contradictions)
+        for dimension_name, dimension_size in zip(item, index_tensor_list[index_tensor_counter].shape):
+            assert (
+                dimension_sizes.get(dimension_name, dimension_size) == dimension_size,
+                f"Incompatible dimensions. You've used {dimension_name!r} in 2 square bracket expressions, and it has 2 different values in those expressions."
+            )
+            dimension_sizes[dimension_name] = dimension_size
+        # If >1 index tensor is being used (e.g. #2b), increment the counter so we compare the next square brackets expression to the right indexing tensor
+        if using_multiple_indices:
+            index_tensor_counter += 1
+    if verbose:
+        print("Dimension sizes:\n  " + "\n  ".join([f"{k}: {v}" for k, v in dimension_sizes.items()]))
+
+
+    # Get dimensions of output, so we know what to broadcast our indices to (when they're strings). This is all the string expressions (added the first time 
+    # they appear), plus the terms in square brackets which don't also appear in string expressions (e.g. as in #4).
+    #   Example #1: ['batch', 'seq', ['batch', 'seq']] -> ['batch', 'seq']
+    #   Example #4: ['batch', ['batch', 'seq', 'k']] -> ['batch', 'seq', 'k']
+    output_dims = []
+    output_shape = []
+    for item in pattern_indices:
+        if isinstance(item, str):
+            output_dims.append(item)
+            output_shape.append(dimension_sizes[item])
+        elif isinstance(item, list):
+            for dim_name in item:
+                if (dim_name not in pattern_indices_str) and (dim_name not in output_dims):
+                    output_dims.append(dim_name)
+                    output_shape.append(dimension_sizes[dim_name])
+    output_shape = tuple(output_shape)
     output_ndim = len(output_shape)
+        
 
-    # Start constructing the index
-    arr_dim_counter = -1
+    # Start constructing the index `full_idx`, by appending t.arange objects or tensors to it
+    output_dim_counter = 0
+    index_tensor_counter = 0
+    full_idx = []
     for (item, dim_size) in zip(pattern_indices, arr.shape):
 
-        # If item is just the name of a dimension, we put a rearranged indices tensor here
-        #   Example #1: "batch" -> we want t.arange(batch_size).reshape(batch_size, 1)
-        #   Example #1: "seq"   -> we want t.arange(seq_len).reshape(1, seq_len)
-        if isinstance(item, str):
-            arr_dim_counter += 1
-            shape = [1] * output_ndim
-            shape[arr_dim_counter] = dim_size
-            full_idx_item = torch.arange(dim_size).reshape(*shape)
+        # ! If item in pattern string is just a str, we put a rearranged indices tensor here so it can broadcast with the index tensors
+        #   Example #1: 'batch' -> torch.arange(batch_size).reshape(batch_size, 1)
+        #   Example #1: 'seq'   -> torch.arange(seq_len).reshape(1, seq_len)
+        #   Example #4: 'batch' -> torch.arange(batch_size).reshape(batch_size, 1, 1)
         
-        # If item is a list, this means we're indexing into the indices using this
-        #   Example #1: ["batch", "seq"]      -> we want indices[:, :]
-        #   Example #2: ["batch", "seq", "0"] -> we want indices[:, :, 0]
-        #   Example #3: ["batch"]             -> we want indices[:].reshape(batch_size, 1) (see long comment below)
+        if isinstance(item, str):
+            shape = [1] * output_ndim
+            shape[output_dim_counter] = dim_size
+            output_dim_counter += 1
+            # ! Append the broadcasted torch.arange object to the full list of indices
+            full_idx.append(torch.arange(dim_size).reshape(*shape))
+        
+        # ! If item is a list, this means we should be indexing into the corresponding index tensor
+        # i.e. we need to construct `idx` and `shape` s.t. index_tensor[idx].reshape(shape) is the thing we want to append to the `full_idx` list
+        #   Example #1:  ['batch', 'seq']      -> we want indices[:, :]
+        #   Example #2a: ['batch', 'seq', '0'] -> we want indices[:, :, 0]
+        #   Example #3:  ['batch']             -> we want indices[:].reshape(batch_size, 1) so it broadcasts with the final output shape
+        
         elif isinstance(item, list):
-            # Get all the "within square brackets" indices, either `:` = slice(None) or ints
+        
+            # Get the correct indices tensor, and increment the counter if we're using multiple indices
+            index_tensor = index_tensor_list[index_tensor_counter]
+            if using_multiple_indices:
+                index_tensor_counter += 1
+        
+            # Get `idx` we'll be using to index our tensor - this will involve either slice(None) or taking a slice e.g. in Example #2a
             idx = [int(i) if i.isdigit() else slice(None) for i in item]
-            # Get the correct indices tensor
-            indices_for_this_item = indices.pop(0)
-            indices_for_this_item_sliced = indices_for_this_item[idx]
-            # If the output shape is larger than the shape of `indices`, we need to reshape `indices` to add dummy dimensions.
-            # e.g. in Example #3, we want our indices to eventually broadcast to our output shape of (batch, d_vocab), and since
-            # `indices` only has shape (batch,), we need to add a dummy dimension for `d_vocab`. Also, we want all dimensions with
-            # integers (like in Example #2) to be dummy dimensions. So we'll start with the full output shape, and then replace
-            # the elements with dummy dimensions so the reshape operation doesn't change the number of elements. 
+        
+            # Get `shape` we'll be using to broadcast our tensor back out to the shape of final output. This is necessary if the indexing
+            # tensor has fewer dims than the final output, e.g. Example #3. We do this by taking the output shape, and replacing the elements
+            # with 1s if they're not dimensions that already exist in `index_tensor[idx]`.
             shape = list(output_shape)
-            for i, p in enumerate(pattern_indices_str):
-                if p not in item:
-                    shape[i] = 1
-            full_idx_item = indices_for_this_item_sliced.reshape(*shape)
-
-        full_idx.append(full_idx_item)
+            for dim_idx, dim_name in enumerate(output_dims):
+                if dim_name not in item:
+                    shape[dim_idx] = 1
+            assert (
+                torch.tensor(shape).prod().item() == index_tensor[idx].numel(),
+                "Something's gone wrong with the shape broadcasting. Please submit an issue at https://github.com/callummcdougall/eindex"
+            )
+            # ! Append the reshaped index tensor to the full list of indices
+            full_idx.append(index_tensor[idx].reshape(*shape))
 
     return arr[full_idx]
