0253192423088321281d563ae590a2ba60fb176a
Callum McDougall <cal.s.mcdougall@gmail.com>
2023-11-27 20:41:49 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        'torch'
+        'torch',
+        'einops'  # Add this line
     ],
     author='Callum McDougall',
-    author_email='cal.s.mcdougall@gmail.com',  # Replace with your email
-    description='My interpretation of indexing with einops-like pattern notation',  # Replace with your project's description
+    author_email='cal.s.mcdougall@gmail.com',
+    description='My interpretation of indexing with einops-like pattern notation',
     url='https://github.com/callummcdougall/eindex',
     classifiers=[
         'Programming Language :: Python :: 3',
