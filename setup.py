695dbb9e9a2a3224c28485fb6e0b8b92c274dcbd
callummcdougall <cal.s.mcdougall@gmail.com>
2024-04-01 14:17:45 +0100

    make into library

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='eindex',
-    version='0.1',
-    packages=find_packages(),
-    install_requires=[
+    name = 'eindex-callum',
+    version  =  '0.1.0',
+    packages = find_packages(),
+    install_requires = [
         'torch',
-        'einops'  # Add this line
+        'einops'
     ],
-    author='Callum McDougall',
-    author_email='cal.s.mcdougall@gmail.com',
-    description='My interpretation of indexing with einops-like pattern notation',
-    url='https://github.com/callummcdougall/eindex',
-    classifiers=[
+    author = 'Callum McDougall',
+    author_email = 'cal.s.mcdougall@gmail.com',
+    description = 'My interpretation of einops-like indexing',
+    url = 'https://github.com/callummcdougall/eindex',
+    classifiers = [
         'Programming Language :: Python :: 3',
         'Operating System :: OS Independent',
     ],
